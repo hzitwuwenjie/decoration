12dcdc4ace660233929fd929a5cd6e2221d78d7d
Administrator <wuwenjie@hzitxx.com>
2018-08-01 22:29:03 +0800

    Initial commit

---
+++ src/main/java/com/hzit/decoration/DecorationApplication.java
@@ -0,0 +1,12 @@
+package com.hzit.decoration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecorationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecorationApplication.class, args);
+	}
+}
