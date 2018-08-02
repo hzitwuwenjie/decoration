08a53c1a5b743133cc23dae4cc9f58c5673413a0
Administrator <wuwenjie@hzitxx.com>
2018-08-02 21:38:31 +0800

    上传页面

--- src/main/java/com/hzit/decoration/DecorationApplication.java
+++ src/main/java/com/hzit/decoration/DecorationApplication.java
@@ -1,9 +1,11 @@
 package com.hzit.decoration;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.hzit.decoration.dao")
 public class DecorationApplication {
 
 	public static void main(String[] args) {
